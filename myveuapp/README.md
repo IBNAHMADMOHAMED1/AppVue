e80858ac4baf8c7e7193195b6c81b718e5fca578
mohamed ibnahmad <ibnahmadmohamed8@gmail.com>
2022-02-27 20:05:02 +0000

    initail commit

--- myveuapp/README.md
+++ myveuapp/README.md
@@ -1,4 +1,4 @@
-# myfristapp
+# myveuapp
 
 ## Project setup
 ```
---
+++ myveuapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1 class="title"> hi mohamed</h1>
+</div>
+
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.title {
+  color:rebeccapurple;
+}
+
+</style>
