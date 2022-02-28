e7e9010fce9eacfc56797619735fb385d7b1f626
mohamed ibnahmad <ibnahmadmohamed8@gmail.com>
2022-02-28 11:33:02 +0000

    add function && bind value

--- myveuapp/src/App.vue
+++ myveuapp/src/App.vue
@@ -1,18 +1,21 @@
+ 
 <template>
 <div id="app">
   <img alt="Vue logo" src="./assets/logo.png">
   <h1 class="title"> hi mohamed</h1>
+  <FirstApp msg='Hello' /> 
+  
 </div>
-
 
 </template>
 
 <script>
-
+ import FirstApp from './components/FirstApp';
+  
 export default {
   name: 'App',
   components: {
-   
+    FirstApp
   }
 }
 </script>
@@ -26,8 +29,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-.title {
-  color:rebeccapurple;
-}
+
 
 </style>
---
+++ myveuapp/src/components/FirstApp.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+         <SecondApp />
+        <div v-bind:class="getStyel()">mohamedibnahmad</div>
+        <h2>{{msg}}</h2>
+        <p>{{nom}}</p>
+
+       
+    </div>
+   
+</template>
+
+<script>
+export default {
+    name: 'FirstApp',
+   props: {
+  msg: String,
+
+},
+data(){
+    return {
+        
+        nom : 'mohamed',
+        className : 'f1'
+        
+    }
+},
+methods : {
+    getStyel(){
+        return 'f1'
+    }
+}
+
+
+}
+        
+    
+
+</script>
+
+<style>
+h1 {
+    color : red ;
+    font-size: 2rem;
+}
+.f1{
+    color:bisque;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 4rem;
+}
+</style>
+
