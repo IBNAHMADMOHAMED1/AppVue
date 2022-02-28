5cb5c54c4c9bcff532f89542407d7fc95688e7b6
mohamed ibnahmad <ibnahmadmohamed8@gmail.com>
2022-02-28 14:59:05 +0000

    condition && lops && model

--- myveuapp/src/components/FirstApp.vue
+++ myveuapp/src/components/FirstApp.vue
@@ -5,9 +5,40 @@
         <div>{{salary}}</div>
         <button v-on:click="inc()">inc</button>
         <button v-on:click="dec()">dec</button>
-
+        <button v-on:keyup.ctrl="click()">enter</button>
+        <input type="text"  v-model="salary">
+        <button v-on:click="getslary()">confirm</button>
         <h2>{{msg}}</h2>
         <p>{{nom}}</p>
+        <input type="checkbox" checked="verif" v-model="verif">
+        <label >{{verif}}</label>
+<br>
+        <select v-model="selected">
+        <option disabled value="">Choisissez</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+        </select>
+        <span>Sélectionné : {{ selected }}</span>
+        <input type="text" v-model="caractere">
+       <div v-show="v==true">oki</div> 
+       <div v-if="caractere=='A'"> Hello A </div>
+       <div v-else-if="caractere=='B'"> Hello B </div>
+        <div v-else-if="caractere=='c'"> Hello c </div>
+        <div v-else> Hello D </div>
+        <ul  v-for="item in country" :key="item.id">
+            <template  v-if="item.id<=1">
+                  <li>{{item.name}}</li>
+            <li>{{item.id}}</li>
+            </template>
+          
+
+        </ul>
+        <ul>
+            <li v-for="item in op" :key="item.id">{{item}}</li>
+        </ul>
+
+
 
        
     </div>
@@ -26,9 +57,15 @@ data(){
         
         nom : 'mohamed',
         className : 'f1',
-        salary : 12
-        
-        
+        salary : 12,
+        verif:false,
+        selected:'',
+        v : false ,
+        caractere :'B',
+        country:[{id:1,name: 'usa'},{id:2,name: 'france'},{id:3,name: 'Uk'},],
+        op:{name: 'dolar',id:12}
+
+
     }
 },
 methods : {
@@ -43,7 +80,16 @@ methods : {
     },
     dec(){
         return this.salary--;
+    },
+    click()
+    {
+        alert('don!');
+
+    },
+    getslary(){
+        console.log(this.salary)
     }
+
 }
 
 
